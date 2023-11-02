1f0888285d8d647983d4be2b695a01d2728db612
Cedricaf <34737@ma-web.nl>
2023-11-02 22:42:12 +0100

    a

--- Db opdrachten/05/dbdata.php
+++ Db opdrachten/05/dbdata.php
@@ -1,13 +1,14 @@
 <?php
 
-$DB_NAME=
-$DB_USER=
-$DB_PASSWORD=
-$DB_HOST=
 
-$envSettings = [];
+    $envSettings = [];
     if (file_exists(dirname(__FILE__).'/.env')) {
         $envSettings = parse_ini_file(dirname(__FILE__).'/.env'); 
     }
+
+    $databaseServer = $envSettings['DB_HOST'];
+    $user = $envSettings['DB_USER'];
+    $password = $envSettings['DB_PASSWORD'];
+    $schema = $envSettings['DB_SCHEMA_NAME'];
 
 ?>
